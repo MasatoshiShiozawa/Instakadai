a92f5455a26167c6550fd661ea290504362c32d9
MasatoshiShiozawa <victorynov21@gmail.com>
2020-10-13 02:09:28 +0000

    Blog.conrollerを修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     @blog = current_user.blogs.build(blog_params)
     if @blog.save
-      ContactMailer.contact_mail(@blog).deliver
+      # ContactMailer.contact_mail(@blog).deliver
       redirect_to blogs_path, notice: "作成しました！"
     else
       # 入力フォームを再描画します。

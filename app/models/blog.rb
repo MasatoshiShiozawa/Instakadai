c8b069f1200ad85c0bc930fefa9d0fc39c9994dd
MasatoshiShiozawa <victorynov21@gmail.com>
2020-10-14 13:32:08 +0000

    インスタ課題再UP

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
     validates :content, presence: true
-    # validates :image, presence: true
+    validates :image, presence: true
     belongs_to :user
     has_many :favorites, dependent: :destroy
     has_many :favorite_users, through: :favorites, source: :user
